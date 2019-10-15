32cb5e2934a6786b0190cae6032302b3bb156cee
hirokazutei <hirokazu.tei@gmail.com>
2019-10-16 08:11:08 +0900

    FEAT: Adding touchableFactory to UIFactory

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,33 +1,38 @@
 /// <reference types="react" />
 import { Themes } from './Theme/types';
-declare function UIFactory<ThemeObject>(themes: Themes<ThemeObject>): {
-    Circular: import("react").FunctionComponent<import("./components/Button/types").ButtonProps<null, {
-        default: import("./components/Button/types").ButtonSizeProps;
-        small: import("./components/Button/types").ButtonSizeProps;
-        tiny: import("./components/Button/types").ButtonSizeProps;
-        medium: import("./components/Button/types").ButtonSizeProps;
-        large: import("./components/Button/types").ButtonSizeProps;
-        huge: import("./components/Button/types").ButtonSizeProps;
-        massive: import("./components/Button/types").ButtonSizeProps;
-    }>>;
-    Round: import("react").FunctionComponent<import("./components/Button/types").ButtonProps<null, {
-        default: import("./components/Button/types").ButtonSizeProps;
-        small: import("./components/Button/types").ButtonSizeProps;
-        tiny: import("./components/Button/types").ButtonSizeProps;
-        medium: import("./components/Button/types").ButtonSizeProps;
-        large: import("./components/Button/types").ButtonSizeProps;
-        huge: import("./components/Button/types").ButtonSizeProps;
-        massive: import("./components/Button/types").ButtonSizeProps;
-    }>>;
-    Sharp: import("react").FunctionComponent<import("./components/Button/types").ButtonProps<null, {
-        default: import("./components/Button/types").ButtonSizeProps;
-        small: import("./components/Button/types").ButtonSizeProps;
-        tiny: import("./components/Button/types").ButtonSizeProps;
-        medium: import("./components/Button/types").ButtonSizeProps;
-        large: import("./components/Button/types").ButtonSizeProps;
-        huge: import("./components/Button/types").ButtonSizeProps;
-        massive: import("./components/Button/types").ButtonSizeProps;
-    }>>;
+export default function UIFactory<ThemeObject>(themes: Themes<ThemeObject>): {
+    Button: {
+        Circular: import("react").FunctionComponent<import("./components/Button/types").ButtonProps<null, {
+            default: import("./components/Button/types").ButtonSizeProps;
+            small: import("./components/Button/types").ButtonSizeProps;
+            tiny: import("./components/Button/types").ButtonSizeProps;
+            medium: import("./components/Button/types").ButtonSizeProps;
+            large: import("./components/Button/types").ButtonSizeProps;
+            huge: import("./components/Button/types").ButtonSizeProps;
+            massive: import("./components/Button/types").ButtonSizeProps;
+        }>>;
+        Round: import("react").FunctionComponent<import("./components/Button/types").ButtonProps<null, {
+            default: import("./components/Button/types").ButtonSizeProps;
+            small: import("./components/Button/types").ButtonSizeProps;
+            tiny: import("./components/Button/types").ButtonSizeProps;
+            medium: import("./components/Button/types").ButtonSizeProps;
+            large: import("./components/Button/types").ButtonSizeProps;
+            huge: import("./components/Button/types").ButtonSizeProps;
+            massive: import("./components/Button/types").ButtonSizeProps;
+        }>>;
+        Sharp: import("react").FunctionComponent<import("./components/Button/types").ButtonProps<null, {
+            default: import("./components/Button/types").ButtonSizeProps;
+            small: import("./components/Button/types").ButtonSizeProps;
+            tiny: import("./components/Button/types").ButtonSizeProps;
+            medium: import("./components/Button/types").ButtonSizeProps;
+            large: import("./components/Button/types").ButtonSizeProps;
+            huge: import("./components/Button/types").ButtonSizeProps;
+            massive: import("./components/Button/types").ButtonSizeProps;
+        }>>;
+    };
+    Touchable: {
+        Filled: import("react").FunctionComponent<import("./components/Touchable/types").TouchableProps<null, import("./components/Button/types").ButtonSizeKeys>>;
+        Bordered: import("react").FunctionComponent<import("./components/Touchable/types").TouchableProps<null, import("./components/Button/types").ButtonSizeKeys>>;
+    };
 };
-export { UIFactory };
 //# sourceMappingURL=index.d.ts.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import {Themes} from './Theme/types';
 import buttonFactory from './components/Button';
+import touchableFactory from './components/Touchable';
 import {DEFAULT_BUTTON_SIZES} from './components/Button/constants';
+import {DEFAULT_TOUCHABLE_SIZES} from './components/Touchable/constants';
+import {TouchablePaddingKeys} from './components/Touchable/types';
 
 type ButtonSizes = typeof DEFAULT_BUTTON_SIZES;
 
-function UIFactory<ThemeObject>(themes: Themes<ThemeObject>) {
+export default function UIFactory<ThemeObject>(themes: Themes<ThemeObject>) {
   const Button = buttonFactory<Themes<ThemeObject>, null, ButtonSizes>({
     themes,
     buttonSizes: DEFAULT_BUTTON_SIZES,
   }); // Button Sizes
-  return Button;
+  const Touchable = touchableFactory<
+    Themes<ThemeObject>,
+    null,
+    TouchablePaddingKeys
+  >({
+    themes,
+    touchablePaddingSizes: DEFAULT_TOUCHABLE_SIZES,
+  });
+  return {Button, Touchable};
 }
-
-export {UIFactory};
