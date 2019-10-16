b676b21ef19c1cc3b27a33dd0f456e9529933757
hirokazutei <hirokazu.tei@gmail.com>
2019-10-16 09:24:20 +0900

    FEAT: Exposing some APIs and Types

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import UIFactory from './uiFactory';
+import {Themes, ThemePalette} from './Theme/types';
+import buttonFactory from './components/Button';
+import {
+  ButtonProps,
+  ButtonShapeOptions,
+  ButtonShapes,
+  ButtonSizeKeys,
+  ButtonSizeProps,
+  ButtonType,
+  ButtonFactoryProps,
+} from './components/Button/types';
+import touchableFactory from './components/Touchable';
+import {
+  TouchableType,
+  TouchableTypeOptions,
+  TouchablePaddingKeys,
+  TouchableProps,
+  TouchablePaddingProps,
+  TouchableFactoryProps,
+  TouchableVerHorPaddingProps,
+  TouchableAllPaddingProps,
+} from './components/Touchable/types';
+
+export {
+  buttonFactory,
+  ButtonProps,
+  ButtonShapeOptions,
+  ButtonShapes,
+  ButtonSizeKeys,
+  ButtonSizeProps,
+  ButtonType,
+  ButtonFactoryProps,
+  ThemePalette,
+  Themes,
+  touchableFactory,
+  TouchableType,
+  TouchableTypeOptions,
+  TouchablePaddingKeys,
+  TouchableProps,
+  TouchablePaddingProps,
+  TouchableFactoryProps,
+  TouchableVerHorPaddingProps,
+  TouchableAllPaddingProps,
+  UIFactory,
+};
