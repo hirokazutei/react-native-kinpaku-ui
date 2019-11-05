739d9ab60332094041fd23ca167127f59116c6c7
hirokazutei <hirokazu.tei@gmail.com>
2019-11-06 08:45:56 +0900

    FEAT: Updating UIFactory

--- src/uiFactory.ts
+++ src/uiFactory.ts
@@ -3,23 +3,38 @@ import buttonFactory from './components/Button';
 import touchableFactory from './components/Touchable';
 import {DEFAULT_BUTTON_SIZES} from './components/Button/constants';
 import {DEFAULT_TOUCHABLE_SIZES} from './components/Touchable/constants';
+import radioButtonFactory from './components/RadioButton';
+import {DEFAULT_RADIO_BUTTON_SIZE} from './components/RadioButton/constants';
+import {Color} from './types';
 
-export default function UIFactory<ThemeObject>(themes: Themes<ThemeObject>) {
+export default function UIFactory<ThemeObject, AdditionalPalettes>(
+  themes: Themes<ThemeObject>,
+  additionalPalettes?: {[key in keyof AdditionalPalettes]: Color},
+) {
+  const commonProps = {themes, additionalPalettes};
   const Button = buttonFactory<
     Themes<ThemeObject>,
-    null,
+    AdditionalPalettes,
     typeof DEFAULT_BUTTON_SIZES
   >({
-    themes,
+    ...commonProps,
     sizes: DEFAULT_BUTTON_SIZES,
-  }); // Button Sizes
+  });
+  const RadioButton = radioButtonFactory<
+    Themes<ThemeObject>,
+    AdditionalPalettes,
+    null
+  >({
+    ...commonProps,
+    sizes: DEFAULT_RADIO_BUTTON_SIZE,
+  });
   const Touchable = touchableFactory<
     Themes<ThemeObject>,
-    null,
+    AdditionalPalettes,
     typeof DEFAULT_TOUCHABLE_SIZES
   >({
-    themes,
+    ...commonProps,
     sizes: DEFAULT_TOUCHABLE_SIZES,
   });
-  return {Button, Touchable};
+  return {Button, RadioButton, Touchable};
 }
