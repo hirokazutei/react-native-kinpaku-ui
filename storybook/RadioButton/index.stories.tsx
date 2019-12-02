385bd0366e1d598957d6bf56766e263f0748659f
hirokazutei <hirokazu.tei@gmail.com>
2019-12-02 09:49:09 +0900

    FEAT: Add story for CheckBox

--- storybook/RadioButton/index.stories.tsx
+++ storybook/RadioButton/index.stories.tsx
@@ -9,7 +9,7 @@ import themes from '../../src/themes';
 import {ThemePalette} from '../../src/Theme/types';
 import {RadioButtonProps} from '../../src/components/RadioButton/types';
 import radioButtonFactory from '../../src/components/RadioButton';
-import {DefaultRadioSizes} from '../../src/components/RadioButton/constants';
+import {DefaultRadioButtonSizes} from '../../src/components/RadioButton/constants';
 
 const {Outline, Reverse, Fill} = radioButtonFactory<
   typeof themes,
@@ -27,8 +27,8 @@ const colorSelect: {[key in keyof ThemePalette]?: keyof ThemePalette} = {
 };
 
 const sizeSelect: {
-  [key in IntersectDefaultKey<DefaultRadioSizes>]?: UnionDefaultKey<
-    DefaultRadioSizes
+  [key in IntersectDefaultKey<DefaultRadioButtonSizes>]?: UnionDefaultKey<
+    DefaultRadioButtonSizes
   >;
 } = {
   small: 'small',
