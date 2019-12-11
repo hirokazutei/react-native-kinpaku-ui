9f6e90a08ffedbe718e37c1e78827b4edfc4f385
hirokazutei <hirokazu.tei@gmail.com>
2019-12-11 09:58:51 +0900

    FEAT: Added colorResolverFactory for easier color resolvement

---
+++ src/helper.ts
@@ -0,0 +1,35 @@
+import {ThemePalette} from './theme/types';
+
+function colorResolverFactory<AdditionalPalettes>({
+  additionalPalettes,
+  currentTheme,
+}: {
+  additionalPalettes?: {
+    [key in keyof AdditionalPalettes]: string;
+  };
+  currentTheme: {
+    [key in keyof ThemePalette]: string;
+  };
+}) {
+  const resolver = ({
+    color,
+    defaultColor,
+  }: {
+    color?: keyof (ThemePalette & AdditionalPalettes);
+    defaultColor: string;
+  }) => {
+    if (color) {
+      if (
+        additionalPalettes &&
+        additionalPalettes[color as keyof AdditionalPalettes]
+      ) {
+        return additionalPalettes[color as keyof AdditionalPalettes];
+      } else if (currentTheme[color as keyof ThemePalette]) {
+        return currentTheme[color as keyof ThemePalette];
+      }
+    }
+    return defaultColor;
+  };
+  return resolver;
+}
+export {colorResolverFactory};
