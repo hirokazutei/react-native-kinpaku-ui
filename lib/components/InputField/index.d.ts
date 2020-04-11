588ff08a6a182faf5f1beb8efc569b7e6b8078c1
Hirokazu Tei <hirokazutei@gmail.com>
2020-04-11 18:43:57 -0400

    CHORE: Generating lib files

--- lib/components/InputField/index.d.ts
+++ lib/components/InputField/index.d.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import { InputFieldFactoryProps, InputFieldProps as Props, InputFieldVariations } from './types';
-declare function inputFieldFactory<Themes, AdditionalPalettes, InputFieldSizes>({ themes, additionalPalettes, sizes, inputFieldType, defaultShape, }: InputFieldFactoryProps<Themes, AdditionalPalettes, InputFieldSizes>): {
+declare function inputFieldFactory<Themes, AdditionalPalettes, InputFieldSizes>({ themes, additionalPalettes, sizes, defaultColor, inputFieldType, defaultShape, }: InputFieldFactoryProps<Themes, AdditionalPalettes, InputFieldSizes>): {
     [key in InputFieldVariations]: React.FunctionComponent<Props<AdditionalPalettes, InputFieldSizes>>;
 };
 export default inputFieldFactory;
