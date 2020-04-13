7d1d0f0cfc0f36654e8ff239a7dc0648ee6f7497
Hirokazu Tei <hirokazutei@gmail.com>
2020-04-12 22:55:19 -0400

    CHORE: Generated lib files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,16 +1,16 @@
 import UIFactory from './uiFactory';
 import { Themes, ThemePalette } from './theme/types';
 import buttonFactory from './components/Button';
-import { ButtonProps, ButtonShapeVariation, ButtonSizeProps, ButtonType, ButtonFactoryProps } from './components/Button/types';
+import { ButtonFactoryProps, ButtonProps, ButtonShapeVariation, ButtonSizeProps, ButtonType } from './components/Button/types';
 import checkBoxFactory from './components/CheckBox';
-import { CheckBoxProps, CheckBoxShapeVariation, CheckBoxSizeProps, CheckBoxType, CheckBoxFactoryProps } from './components/CheckBox/types';
+import { CheckBoxFactoryProps, CheckBoxProps, CheckBoxShapeVariation, CheckBoxSizeProps, CheckBoxType } from './components/CheckBox/types';
 import inputFieldFactory from './components/InputField';
-import { InputFieldFactoryProps, InputFieldProps, InputFieldShape, InputFieldSizeProps, InputFieldType, InputFieldVariationProps, InputFieldVariation } from './components/InputField/types';
+import { InputFieldFactoryProps, InputFieldProps, InputFieldShape, InputFieldSizeProps, InputFieldType, InputFieldVariation, InputFieldVariationProps } from './components/InputField/types';
 import touchableFactory from './components/Touchable';
-import { TouchableTypeVariations, TouchableProps, TouchableSizeProps, TouchableFactoryProps, TouchableVerHorSizeProps, TouchableAllSizeProps } from './components/Touchable/types';
+import { TouchableAllSizeProps, TouchableFactoryProps, TouchableProps, TouchableSizeProps, TouchableTypeVariations, TouchableVerHorSizeProps } from './components/Touchable/types';
 import radioButtonFactory from './components/RadioButton';
 import { RadioButtonFactoryProps, RadioButtonProps, RadioButtonSizeProps, RadioButtonTypeVariation } from './components/RadioButton/types';
 import textFactory from './components/Text';
-import { TextSizeProps, TextVariationProps, TextFactoryProps, TextProps } from './components/Text/types';
-export { buttonFactory, ButtonFactoryProps, ButtonProps, ButtonSizeProps, ButtonType, ButtonShapeVariation, checkBoxFactory, CheckBoxProps, CheckBoxShapeVariation, CheckBoxSizeProps, CheckBoxType, CheckBoxFactoryProps, radioButtonFactory, InputFieldFactoryProps, InputFieldProps, InputFieldShape, InputFieldSizeProps, InputFieldType, InputFieldVariationProps, InputFieldVariation, inputFieldFactory, RadioButtonFactoryProps, RadioButtonProps, RadioButtonSizeProps, RadioButtonTypeVariation, ThemePalette, Themes, textFactory, TextFactoryProps, TextProps, TextSizeProps, TextVariationProps, TouchableAllSizeProps, touchableFactory, TouchableFactoryProps, TouchableProps, TouchableSizeProps, TouchableTypeVariations, TouchableVerHorSizeProps, UIFactory, };
+import { TextFactoryProps, TextProps, TextSizeProps, TextVariationProps } from './components/Text/types';
+export { ButtonFactoryProps, ButtonProps, ButtonType, ButtonShapeVariation, ButtonSizeProps, buttonFactory, CheckBoxFactoryProps, CheckBoxProps, CheckBoxShapeVariation, CheckBoxSizeProps, CheckBoxType, checkBoxFactory, InputFieldFactoryProps, InputFieldProps, InputFieldShape, InputFieldSizeProps, InputFieldType, InputFieldVariationProps, InputFieldVariation, inputFieldFactory, RadioButtonFactoryProps, RadioButtonProps, RadioButtonSizeProps, RadioButtonTypeVariation, radioButtonFactory, ThemePalette, Themes, TextFactoryProps, TextProps, TextSizeProps, TextVariationProps, textFactory, TouchableAllSizeProps, TouchableFactoryProps, TouchableProps, TouchableSizeProps, TouchableTypeVariations, TouchableVerHorSizeProps, touchableFactory, UIFactory, };
 //# sourceMappingURL=index.d.ts.map
