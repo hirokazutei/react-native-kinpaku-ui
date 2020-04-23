8c739c47e776dfda70fea6cb0ee96b94785947e5
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-04-23 09:44:12 -0400

    CHORE: Generated lib file

--- lib/components/Touchable/constants.d.ts
+++ lib/components/Touchable/constants.d.ts
@@ -1,12 +1,12 @@
 import { FlexAlignType } from 'react-native';
 import { UnionDefaultKey } from '../../types';
-import { TouchableAllSizeProps, TouchableTypeVariations } from './types';
+import { TouchableAllSizeProps, TouchableShapeVariation } from './types';
 declare type DefaultTouchableSize = 'tiny' | 'small' | 'medium' | 'large' | 'huge' | 'massive';
 declare const DEFAULT_TOUCHABLE_SIZE: {
     [key in UnionDefaultKey<DefaultTouchableSize>]: TouchableAllSizeProps;
 };
 declare const DEFAULT_TOUCHABLE_ALIGN: FlexAlignType;
 declare const DEFAULT_TOUCHABLE_BORDER_WIDTH: number;
-declare const TOUCHABLE_TYPE_VARIATION_KEYS: Array<TouchableTypeVariations>;
-export { DEFAULT_TOUCHABLE_ALIGN, DEFAULT_TOUCHABLE_BORDER_WIDTH, DEFAULT_TOUCHABLE_SIZE, DefaultTouchableSize, TOUCHABLE_TYPE_VARIATION_KEYS, };
+declare const TOUCHABLE_SHAPE_VARIATION_KEYS: Array<TouchableShapeVariation>;
+export { DEFAULT_TOUCHABLE_ALIGN, DEFAULT_TOUCHABLE_BORDER_WIDTH, DEFAULT_TOUCHABLE_SIZE, DefaultTouchableSize, TOUCHABLE_SHAPE_VARIATION_KEYS, };
 //# sourceMappingURL=constants.d.ts.map
--- lib/components/Touchable/index.d.ts
+++ lib/components/Touchable/index.d.ts
@@ -1,9 +1,9 @@
 import React from 'react';
 import { OptionalExistCondition } from '../../types';
-import { TouchableFactoryProps, TouchableProps as Props, TouchableTypeVariations } from './types';
+import { TouchableFactoryProps, TouchableProps as Props, TouchableShapeVariation } from './types';
 import { DEFAULT_TOUCHABLE_SIZE } from './constants';
-declare function touchableFactory<Themes, AdditionalPalettes, TouchableSize, AllowCustomProps>({ themes, sizes, additionalPalettes, }: TouchableFactoryProps<Themes, AdditionalPalettes, TouchableSize, AllowCustomProps>): {
-    [key in TouchableTypeVariations]?: React.FunctionComponent<Props<AdditionalPalettes, OptionalExistCondition<TouchableSize, TouchableSize, typeof DEFAULT_TOUCHABLE_SIZE>, AllowCustomProps>>;
+declare function touchableFactory<Themes, AdditionalPalettes, TouchableSize, AllowCustomProps>({ themes, sizes, additionalPalettes, defaultType, }: TouchableFactoryProps<Themes, AdditionalPalettes, TouchableSize, AllowCustomProps>): {
+    [key in TouchableShapeVariation]: React.FunctionComponent<Props<AdditionalPalettes, OptionalExistCondition<TouchableSize, TouchableSize, typeof DEFAULT_TOUCHABLE_SIZE>, AllowCustomProps>>;
 };
 export default touchableFactory;
 //# sourceMappingURL=index.d.ts.map
