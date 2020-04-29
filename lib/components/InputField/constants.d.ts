ee6bf26ee574b5cc7b732c21794552f715e3cc45
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-04-29 08:06:40 -0400

    CHORE: Generated lib code

--- lib/components/InputField/constants.d.ts
+++ lib/components/InputField/constants.d.ts
@@ -1,9 +1,9 @@
 import { UnionDefaultKey } from '../../types';
 import { InputFieldSizeProps, InputFieldVariation, InputFieldVariationProps, InputFieldShape } from './types';
-declare type DefaultInputFieldSize = 'medium';
+declare type DefaultInputFieldSize = 'small' | 'medium' | 'large';
 declare const DEFAULT_INPUT_FIELD_SIZE: Record<UnionDefaultKey<DefaultInputFieldSize>, InputFieldSizeProps>;
 declare const DEFAULT_INPUT_FIELD_BORDER_WIDTH = 2;
 declare const DEFAULT_INPUT_VARIATION_SETTING: Record<InputFieldVariation, InputFieldVariationProps>;
 declare const INPUT_FIELD_SHAPE: Array<InputFieldShape>;
-export { DEFAULT_INPUT_FIELD_BORDER_WIDTH, DEFAULT_INPUT_FIELD_SIZE, DEFAULT_INPUT_VARIATION_SETTING, INPUT_FIELD_SHAPE, InputFieldVariation, };
+export { DEFAULT_INPUT_FIELD_BORDER_WIDTH, DEFAULT_INPUT_FIELD_SIZE, DEFAULT_INPUT_VARIATION_SETTING, INPUT_FIELD_SHAPE, InputFieldVariation, DefaultInputFieldSize, };
 //# sourceMappingURL=constants.d.ts.map
